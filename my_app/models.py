b09481fdae2effcd315b5c7b1380568f24b8e1ea
nikotin18 <nikita.kladov@gmail.com>
2020-12-08 21:36:40 +0500

    int

--- my_app/models.py
+++ my_app/models.py
@@ -7,7 +7,7 @@ class Client(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     middle_name = models.CharField(max_length=30, null=True, blank=True)
-    phone_number = models.PositiveBigIntegerField()
+    phone_number = models.PositiveIntegerField()
 
     def __str__(self):
         return "%s, %s" % (self.first_name, self.last_name)
